5859d0d1a48230a98ad9f4c1a9749a200e1d11a9
ext.aolani.eslava <aolanieslava1305@gmail.com>
2026-04-05 23:26:39 -0500

    implememtaciones del registro y login y eliminar platillos desde el front

---
+++ Models/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace LaAbejita.Models
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public RolUsuarioLogin Rol { get; set; }
+        public string Contrasena { get; set; }
+    }
+    public enum RolUsuarioLogin
+    {
+        Administrador = 1,
+        Cocinero = 2
+    }
+}
